b0247034b52c20eba288395d9508a4d88fcf0fcb
aminwhat <m.aminwhat@gmail.com>
2024-02-24 01:51:48 +0330

    random shitfing map

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::{
-    io::{stdout, Result, Stdout, Write},
-    time::Duration,
-};
-
+use core::time;
 use crossterm::{
     cursor::{Hide, MoveTo, Show},
     event::{poll, read, Event, KeyCode},
     style::Print,
     terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType},
     ExecutableCommand, QueueableCommand,
+};
+use rand::{thread_rng, Rng};
+use std::{
+    io::{stdout, Result, Stdout, Write},
+    thread,
+    time::Duration,
 };
 
 struct World {
@@ -18,6 +20,8 @@ struct World {
     maxl: u16,
     map: Vec<(u16, u16)>,
     died: bool,
+    next_start: u16,
+    next_end: u16,
 }
 
 fn draw(mut sc: &Stdout, world: &World) -> std::io::Result<()> {
@@ -41,10 +45,40 @@ fn draw(mut sc: &Stdout, world: &World) -> std::io::Result<()> {
 }
 
 fn physics(mut world: World) -> Result<World> {
+    // check if player died
     if world.player_c <= world.map[world.player_l as usize].0
         || world.player_c >= world.map[world.player_l as usize].1
     {
         world.died = true;
+    }
+
+    // shift the map
+    for l in (0..world.map.len() - 1).rev() {
+        world.map[l + 1] = world.map[l];
+    }
+    if world.next_end < world.map[0].1 {
+        world.map[0].1 -= 1;
+    }
+    if world.next_end > world.map[0].1 {
+        world.map[0].1 += 1
+    }
+    if world.next_start < world.map[0].0 {
+        world.map[0].0 -= 1;
+    }
+    if world.next_start > world.map[0].0 {
+        world.map[0].0 += 1
+    }
+
+    let mut rng = thread_rng();
+    if rng.gen_range(0..10) > 7 {
+        // TODOL possible bug: might go out of range
+        if world.next_start == world.map[0].0 && world.next_end == world.map[0].1 {
+            world.next_start = rng.gen_range(world.map[0].0 - 5..world.map[0].1 - 5);
+            world.next_end = rng.gen_range(world.map[0].0 + 5..world.map[0].1 + 5);
+            if world.next_end - world.next_start <= 3 {
+                world.next_start -= 3;
+            }
+        }
     }
     Ok(world)
 }
@@ -64,11 +98,16 @@ fn main() -> std::io::Result<()> {
         maxl: maxl,
         map: vec![((maxc / 2) - 5, (maxc / 2) + 5); maxl as usize],
         died: false,
+        next_end: maxc / 2 + 10,
+        next_start: maxc / 2 - 10,
     };
 
     while !world.died {
         if poll(Duration::from_millis(10))? {
             let key = read().unwrap();
+            while poll(Duration::from_millis(0)).unwrap() {
+                let _ = read();
+            }
             match key {
                 Event::Key(event) => match event.code {
                     KeyCode::Char('q') => break,
@@ -103,6 +142,7 @@ fn main() -> std::io::Result<()> {
         world = physics(world).unwrap();
 
         draw(&sc, &world)?;
+        thread::sleep(time::Duration::from_millis(100));
     }
     // TODO: check for died and show a message
 
